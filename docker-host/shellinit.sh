0ca054de9688af887a88278ce2d8ea2c93e1da1b
Adé Mochtar <amochtar@xebia.com>
2015-03-09 20:28:42 +0100

    Typo in shellinit script

--- docker-host/shellinit.sh
+++ docker-host/shellinit.sh
@@ -1,6 +1,6 @@
-IP_ETH0=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1'})
+IP_ETH1=$(ifconfig eth1 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1'})
 echo 'Boot2docker box created. To access with local docker client use settings below:'
 echo ''
-echo "  export DOCKER_HOST=tcp://${IP_ETH0}:2375"
+echo "  export DOCKER_HOST=tcp://${IP_ETH1}:2375"
 echo '  unset DOCKER_TLS_VERIFY'
 echo ''
